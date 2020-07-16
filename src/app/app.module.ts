800ebe91f51d3625bbabb5a01ffc1f0b9db1ec67
ikhlasse boukhencha <i.boukhencha@esi-sba.dz>
2020-07-16 17:38:17 +0200

    regler register path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-
 import { HomeModule } from './home/home.module';
 
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeRoutingModule } from '../home/home-routing.module';
+import {FormsModule} from '@angular/forms';
 import {HomeComponent} from './home.component';
 import { EssaiComponent } from '../essai/essai.component';
 import { Essai2Component } from './essai2/essai2.component';
@@ -26,6 +27,7 @@ import { ProductComponent } from '../product/product.component';
     HomeRoutingModule,
     CommonModule,
     RouterModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
