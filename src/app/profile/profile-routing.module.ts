42c63f6a77a989a9fa82d4ec2b93a87c1eafa611
fatyyagoub <f.yagoub@esi-sba.dz>
2020-07-23 17:58:52 +0100

    3 new components created in profile (myorders mycart settings)

---
+++ src/app/profile/profile-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {ProfileComponent} from './profile.component';
+import { EssaiComponent } from '../essai/essai.component';
+import { PanierComponent } from '../panier/panier.component';
+import { RegisterComponent } from '../register/register.component';
+import { ProductComponent } from '../product/product.component';
+import{NewsComponent} from  './news/news.component';
+import{PromotionComponent} from  './promotion/promotion.component';
+import { OrderComponent } from '../order/order.component';
+import { ProfileComponent } from '../profile/profile.component';
+
+
+
+
+
+ const routes: Routes = [{ path: 'profile', component: ProfileComponent, children: [
+                              {path: 'home/essai', component: EssaiComponent},
+                              {path: '', component: NewsComponent},
+                              {path: 'promotion', component: PromotionComponent}
+                            ]
+                          },
+                          {path:"login",component:RegisterComponent},
+                          {path:'card',component: PanierComponent},
+                         
+                          {path:'product',component:ProductComponent},
+                          {path:'order',component:OrderComponent},
+                          {path:'profile',component:ProfileComponent}
+                      
+                        ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
