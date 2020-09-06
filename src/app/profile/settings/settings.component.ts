e184d2b5a8209e514bf7d52b5efdccf5f504ac9e
fatyyagoub <f.yagoub@esi-sba.dz>
2020-09-06 14:15:17 +0100

    edit username input

--- src/app/profile/settings/settings.component.ts
+++ src/app/profile/settings/settings.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  public val : boolean=true;
+  public edit(){
+    if (this.val==true){
+      this.val=false;
+    }
+  }
+
+  public save(){
+    this.val=true;
+  }
+
 
   constructor() { }
 
