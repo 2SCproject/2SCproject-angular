bb647bbd549f0260381f3f9ecf1f3af8e8701252
fatyyagoub <f.yagoub@esi-sba.dz>
2020-10-08 23:08:33 +0100

    page profile settings finie

--- src/app/profile/settings/settings.component.ts
+++ src/app/profile/settings/settings.component.ts
@@ -6,15 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  public val : boolean=true;
-  public edit(){
-    if (this.val==true){
-      this.val=false;
+  public val1 : boolean=true;
+  public val2 : boolean=true;
+  public val3 : boolean=true;
+  public val4 : boolean=true;
+  public val5 : boolean=true;
+
+
+
+  public editname(){
+    if (this.val1==true){
+      this.val1=false;
     }
   }
+  public savename(){
+    this.val1=true;
+  }
 
-  public save(){
-    this.val=true;
+
+  public editemail(){
+    if (this.val2==true){
+      this.val2=false;
+    }
+  }
+  public saveemail(){
+    this.val2=true;
+  }
+
+
+  public editpassword(){
+    if (this.val3==true){
+      this.val3=false;
+    }
+  }
+  public savepassword(){
+    this.val3=true;
+  }
+
+  public editphone(){
+    if (this.val4==true){
+      this.val4=false;
+    }
+  }
+  public savephone(){
+    this.val4=true;
+  }
+
+  public editadress(){
+    if (this.val5==true){
+      this.val5=false;
+    }
+  }
+  public saveadress(){
+    this.val5=true;
   }
 
 
